4234081affaadeb944097d12742f2372389ba499
alexa <alexanderkulnyow@yahoo.com>
2020-09-16 20:51:01 +0300

    init

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -269,3 +269,20 @@ if ( ! function_exists( 'arena_woocommerce_header_cart' ) ) {
 	}
 }
 
+add_filter ( 'wc_add_to_cart_message_html', 'wc_add_to_cart_message_filter', 10, 2 );
+
+function wc_add_to_cart_message_filter($message, $product_id = null) {
+	$titles[] = get_the_title( $product_id );
+
+	$titles = array_filter( $titles );
+	$added_text = sprintf( _n( '%s has been added to your cart.', '%s have been added to your cart.', sizeof( $titles ), 'woocommerce' ), wc_format_list_of_items( $titles ) );
+
+	$message = sprintf( '%s <a href="%s" class="button">%s</a>',
+		esc_html( $added_text ),
+//		esc_url( wc_get_page_permalink( 'checkout' ) ),
+//		esc_html__( 'Checkout', 'woocommerce' ),
+		esc_url( wc_get_page_permalink( 'cart' ) ),
+		esc_html__( 'Перейти в корзину', 'woocommerce' ));
+
+	return $message;
+}
